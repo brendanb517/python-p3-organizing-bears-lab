e44fd6e0e175fdc96fb0283778b0c4423b75d263
Brendan <brendan.b517@gmail.com>
2023-04-30 23:40:33 -0400

    Passed all tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 15, "male", "black", "rowdy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 19, "male", "dark brown", "boastful", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "female", "dark brown", "playful", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 40, "male", "grey brown", "weary", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 26, "female", "grey black", "calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 31, "male", "grey", "irritable", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 16, "female", "light brown", "outgoing", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "female", "black", "fearful", TRUE);
